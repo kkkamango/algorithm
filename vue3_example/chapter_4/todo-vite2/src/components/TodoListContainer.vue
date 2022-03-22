da24e66b360219e5e36aa65f0de464a1e50e1862
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-03-22 16:08:32 +0900

    간단한 데이터 저장소 구현

---
+++ vue3_example/chapter_4/todo-vite2/src/components/TodoListContainer.vue
@@ -0,0 +1,26 @@
+<template>
+    
+</template>
+
+<script>
+import {ref, readonly, provide} from 'vue'
+
+export default{
+  name : 'TodoListContainer',
+  setup(props, context) {
+    const todos = ref([]);
+    
+    const initTotos = (init_todos) => {
+      todos.value = init_todos;
+    };
+    const addTodo = (todo, date) => {};
+    const removeTodo = (id) => {};
+    const completeTodo = (id) => {};
+
+    provide('todos', readonly(todos));
+    provide('addTodo', addTodo);
+    provide('removeTodo', removeTodo);
+    provide('completeTodo', completeTodo);
+  }
+}
+</script>
