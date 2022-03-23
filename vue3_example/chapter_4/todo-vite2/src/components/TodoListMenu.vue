2764b012c0b5c52002d9eea38f8c5059bfb6df73
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-03-23 11:02:27 +0900

    TodoList에 전달할 값을 결정하는 TodoListMenu

---
+++ vue3_example/chapter_4/todo-vite2/src/components/TodoListMenu.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <span class="bgblue">&nbsp;</span>&nbsp;
+      <strong>{{state}}</strong>
+    </div>
+    <div class="col">
+      <div class="btn-group float-end">
+        <button class="btn btn-sm dropdown-toggle" data-bs-toggle="dropdown">리스트 필터</button>
+        <ul class="dropdown-menu dropdown-menu-end">
+          <li v-for="key in Object.keys(filters)" :key="key">
+            <a @click="filter = key" class="dropdown-item">
+              {{filters[key].str}}
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {ref, inject, watch, computed} from 'vue'
+
+export default{
+  name : 'TodoListMenu',
+  emits : ['change-filter'], // emit을 사용하기 위해,  이벤트명 선언
+  setup(props, context) {
+    const filters = inject('filter');
+    const filter = ref(0);
+    const state = computed(() => filters[filter.value].str) // 계산된 속성
+    watch( // filter 변경되면 emit을 이용해 부모 컴포넌트(TodoListMain)에게 전달
+      () => filter.value, (filter) => context.emit('change-filter', filter)
+    )
+
+    return {
+      state,
+      filters,
+      filter
+    }
+  }
+}
+</script>
+<style scoped>
+  .bgblue{background-color: blue;}
+</style>
