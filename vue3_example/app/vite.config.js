6a4ecf919cb1832149c6d28c5d250e60514ad234
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-03-29 17:37:14 +0900

    Alias 사용

---
+++ vue3_example/app/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // alias 사용
+  resolve :  {
+    alias : {
+      '/@' : path.resolve(__dirname, './src'),
+      '/@components' : path.resolve(__dirname, './src/components'),
+      '/@app_modules' : path.resolve(__dirname, './src/modules'),
+      '/@store' : path.resolve(__dirname, './src/store'),
+    }
+  },
+  plugins: [vue()]
+})
