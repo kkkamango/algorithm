26966708e029c17498df52e865d1a6ee4d0813e5
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-03-29 12:04:07 +0900

    5-3 상단메뉴 구성

---
+++ vue3_example/app/src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import NavBarName from './components/NavBar.vue'
+
+export default {
+  name : 'App',
+  components : {
+    NavBarName
+  }
+}
+</script>
+
+<template>
+  <NavBarName />
+</template>
