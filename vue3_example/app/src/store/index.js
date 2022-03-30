96629d2b3d85c1e8cd7721f2c3cd95c80d7d0382
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-03-30 16:44:37 +0900

    vuex를 이용하여 전역 스토어 생성

---
+++ vue3_example/app/src/store/index.js
@@ -0,0 +1,12 @@
+/**
+ * /store/index.js
+ * 스토어 모듈을 묶어 하나의 스토어 인스턴스로 만들어 줌
+ */
+import { createStore } from "vuex";
+import {about_me} from './modules/about-me'
+
+export const store = createStore({ // store 생성 (vuex4)
+  modules : {
+    about_me
+  }
+});
