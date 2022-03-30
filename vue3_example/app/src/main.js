8444a3f3f907db5f743f6ec87a2149579010645d
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-03-30 16:44:58 +0900

    vuex를 이용하여 전역 스토어 생성

--- vue3_example/app/src/main.js
+++ vue3_example/app/src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {store} from '/@store/index.js'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.esm.js'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(store); // 플러그인 등록
+app.mount('#app')
