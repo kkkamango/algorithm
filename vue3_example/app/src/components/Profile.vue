26811bea5c03187d38159246da96989a78fcab1a
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-05 23:59:23 +0900

    Card 컴포넌트가 포함된 Profile 컴포넌트

--- vue3_example/app/src/components/Profile.vue
+++ vue3_example/app/src/components/Profile.vue
@@ -1,3 +1,41 @@
+<template>
+  <!-- 타임라인을 표현한 템플릿 코드 -->
+  <div class="container pt-5">
+    <div>
+      <p class="fs-1 mb-0">{{user_data.name}}</p>
+    </div>
+    <div>
+      <p class="fs-4">{{user_data.email}}</p>
+    </div>
+
+    <div v-for="(d, index) in user_data.resume" :key="index"
+      class="row no-gutters">
+      <!-- 카드가 좌우로 표현
+      top : 현재 이력
+      -->
+      <Card :top="!index" :resume="d" :show="index % 2 === 1" />
+      <div class="col-sm-2 text-center flex-column d-none d-sm-flex">
+        <!-- 연도, 선을 표시할 영역 -->
+        <div class="row h-50">
+          <div class="col" :class="[index ? 'border-right' : '']"></div>
+          <div class="col"></div>
+        </div>
+        <h1>
+          <span class="badge badge-pill border"
+            :class="[!index ? 'bg-primary text-light' : 'bg-light text-dark']">
+            {{new Date(d.date).getFullYear()}}
+          </span>
+        </h1>
+        <div class="row h-50">
+          <div class="col" :class="[index != user_data.resume.length - 1 ? 'border-right' : '']"></div>
+          <div class="col"></div>
+        </div>
+      </div>
+      <Card :top="!index" :resume="d" :show="index % 2 === 0" />
+    </div>
+  </div>
+</template>
+
 <script>
   import {defineComponent, computed} from 'vue'
   import {useStore} from 'vuex'
@@ -53,21 +91,4 @@
     },
     components : { Card }
   }
-
-  /*
-  <template>
-    <!-- 타임라인을 표현한 템플릿 코드 -->
-    <div class="container pt-5">
-      <div v-for="(d, index) in user_data.resume" :key="index"
-        class="row no-gutters">
-        <!-- 카드가 좌우로 표현
-        top : 현재 이력
-        -->
-        <Card :top="!index" :resume="d" :show="index % 2 === 1" />
-        <div col-sm-2 /><!-- 연도, 선을 표시할 영역 -->
-        <Card :top="!index" :resume="d" :show="index % 2 === 0" />
-      </div>
-    </div>
-  </template>
-  */
 </script>
