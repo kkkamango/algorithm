723f4539c5988fae34429429b19e3dcd9eff5272
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-05 23:16:17 +0900

    store에 profile 추가

--- vue3_example/app/src/store/modules/about-me.js
+++ vue3_example/app/src/store/modules/about-me.js
@@ -6,14 +6,22 @@ export const about_me = {
   state : () => ({ // state : 저장할 데이터 객체 반환
     name : null,
     email : null,
+    resume : [],
   }),
   getters : { // getters : state의 데이터를 필요한 형식으로 전처리 해주는 반응형 속성
-    name : (state) => {
-      return state.name;
-    },
-    email : (state) => {
-      return state.email;
-    }, 
+    user_data : (state)=>{
+      return {
+        name : state.name,
+        email : state.email,
+        resume : state.resume,
+      }
+    }
+    // name : (state) => {
+    //   return state.name;
+    // },
+    // email : (state) => {
+    //   return state.email;
+    // }, 
   },
   mutations : { // mutations : state 내 데이터 변형할 수 있는 속성
     SET_DATA(state, data) {
