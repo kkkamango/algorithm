20914cf8fdc87f60d229f872023522d726bb91d9
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-05 23:46:57 +0900

    Card 컴포넌트가 포함된 Profile 컴포넌트

---
+++ vue3_example/app/src/components/Profile.vue
@@ -0,0 +1,73 @@
+<script>
+  import {defineComponent, computed} from 'vue'
+  import {useStore} from 'vuex'
+  import useAxios from '/@app_modules/axios.js'
+
+  // Card 컴포넌트
+  const Card = defineComponent({
+    name : 'Card',
+    props : {
+      top : Boolean,
+      resume : Object,
+      show : Boolean,
+    },
+    setup(props){},
+    template : `
+    <div v-if="show" class="col-sm py-2">
+      <div class="card" :class="{'border-primary bg-primary text-light' : top, shadow : top}">
+        <div class="card-body">
+          <h4 class="card-title" :class="[top ? 'text-light' : 'text-muted']">
+            {{resume.title}}
+            <hr>
+            <p class="card-text">{{resume.content}}</p>
+            <a v-if="resume.URL != 'null'" :href="resume.URL" target="_blank"
+              :class="[top ? 'text-light' : 'text-primary']">link</a>
+          </h4>
+        </div>
+      </div>
+    </div>
+    <div v-if="!show" class="col-sm"></div>
+    `
+  });
+
+
+  export default {
+    name : 'Profile',
+    setup() {
+      const store = useStore();
+      const { axiosGet } = useAxios();
+
+      const onSuccess = (data) => {
+        store.dispatch('setAboutMeData', data.data);
+      }
+
+      axiosGet('/db/about-me', onSuccess);
+
+      const user_data = computed(
+        () => store.getters['about_me/user_data']
+      );
+
+      return {
+        user_data,
+      }
+    },
+    components : { Card }
+  }
+
+  /*
+  <template>
+    <!-- 타임라인을 표현한 템플릿 코드 -->
+    <div class="container pt-5">
+      <div v-for="(d, index) in user_data.resume" :key="index"
+        class="row no-gutters">
+        <!-- 카드가 좌우로 표현
+        top : 현재 이력
+        -->
+        <Card :top="!index" :resume="d" :show="index % 2 === 1" />
+        <div col-sm-2 /><!-- 연도, 선을 표시할 영역 -->
+        <Card :top="!index" :resume="d" :show="index % 2 === 0" />
+      </div>
+    </div>
+  </template>
+  */
+</script>
