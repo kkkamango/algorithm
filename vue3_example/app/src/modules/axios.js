9d56b35fd223e20ec23d34ad2e7d8899507ef40d
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-08 08:49:21 +0900

    Update axios.js

--- vue3_example/app/src/modules/axios.js
+++ vue3_example/app/src/modules/axios.js
@@ -1,9 +1,9 @@
-import {axios} from 'axios'
+import axios from 'axios'
 
 export default function (){
   const BASE_URL = 'http://localhost:8000';
   const axiosGet = (URL, onSuccess = null, onFail = null) => {
-    const final_url = URL.startWith('http') ? URL : BASE_URL + URL;
+    const final_url = URL.startsWith('http') ? URL : BASE_URL + URL;
 
     axios.get(final_url)
       .then(resp => {
