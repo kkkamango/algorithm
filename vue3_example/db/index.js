9d8ad2fda4f648909d007514e02d02fddd9c1416
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-08 08:48:11 +0900

    프로필 관련 db 정보 변경

--- vue3_example/db/index.js
+++ vue3_example/db/index.js
@@ -19,42 +19,44 @@ let db = new sqlite3.Database( 'database.db', (err) => {
         )
       }
     );
+
+    db.run(
+      `CREATE TABLE IF NOT EXISTS TBL_MY_RESUME (DATE DATE, TITLE TEXT, CONTENT TEXT, URL TEXT,
+        UNIQUE(DATE, TITLE))`,
+        (err2) => {
+          if (!err2){
+            const resume = [
+              {
+                date : '1986-01-23',
+                title : '출생',
+                content : '생일 축하 합니다~!',
+                URL : null
+              }, {
+                date : '2014-04-01',
+                title : '개발자로 시작',
+                content : '새로운 커리어로 도전',
+                URL : null
+              }, {
+                date : '2021-06-07',
+                title : 'github 시작',
+                content : '개발 관련 스터디를 위한 github 시작',
+                URL : 'https://github.com/kkkamango'
+              },          
+            ];
+    
+            resume.forEach(item => {
+              const query = `INSERT OR IGNORE INTO TBL_MY_RESUME 
+                (DATE, TITLE, CONTENT, URL) 
+                VALUES('${item.date}', '${item.title}', '${item.content}', '${item.URL}')`;
+              db.run(query);
+            });
+          }
+        }
+    );
   }
 });
 
-db.run(
-  `CREATE TABLE IF NOT EXISTS TBL_MY_RESUME (DATE DATE, TITLE TEXT, CONTENT TEXT, URL TEXT,
-    UNIQUE(DATE, TITLE))`,
-    (err2) => {
-      if (!err2){
-        const resume = [
-          {
-            date : '1986-01-23',
-            title : '출생',
-            content : '생일 축하 합니다~!',
-            URL : null
-          }, {
-            date : '2014-04-01',
-            title : '개발자로 시작',
-            content : '새로운 커리어로 도전',
-            URL : null
-          }, {
-            date : '2021-06-07',
-            title : 'github 시작',
-            content : '개발 관련 스터디를 위한 github 시작',
-            URL : 'https://github.com/kkkamango'
-          },          
-        ];
 
-        resume.forEach(item => {
-          const query = `INSERT OR IGNORE INTO TBL_MY_RESUME 
-            (DATE, TITLE, CONTENT, URL) 
-            VALUES('${item.date}', '${item.title}', '${item.content}', '${item.URL}')`;
-          db.run(qurey);
-        });
-      }
-    }
-);
 
 app.listen(PORT, () => {
   console.log(`Listening... ${PORT}`);
@@ -84,20 +86,20 @@ app.get('/db/about-me', (req, res, next) => {
     }
   });
 
-  db.get("SELECT * FROM TBL_ABOUT_MYSELF", (err, row) => {
-    if (!err) {
-      result.data = row;
-      db.all('SELECT * FROM TBL_MY_RESUME ORDER BY DATE DESC', (err2, rows) => {
-        if (!err2){
-          result.rsp = 'ok';
-          result.data.resume = rows;
-          res.json(result);
-        } else {
-          res.json(result);
-        }
-      });
-    } else {
-      res.json(result);
-    }
-  })
+  // db.get("SELECT * FROM TBL_ABOUT_MYSELF", (err, row) => {
+  //   if (!err) {
+  //     result.data = row;
+  //     db.all('SELECT * FROM TBL_MY_RESUME ORDER BY DATE DESC', (err2, rows) => {
+  //       if (!err2){
+  //         result.rsp = 'ok';
+  //         result.data.resume = rows;
+  //         res.json(result);
+  //       } else {
+  //         res.json(result);
+  //       }
+  //     });
+  //   } else {
+  //     res.json(result);
+  //   }
+  // })
 });
