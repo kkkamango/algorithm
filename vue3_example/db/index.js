d7803a50051272c08688ec233fe2ed3d3370255b
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-11 17:42:51 +0900

    5-9 어플리케이션 소개 페이지

--- vue3_example/db/index.js
+++ vue3_example/db/index.js
@@ -1,105 +1,29 @@
 const sqlite3 = require('sqlite3');
 const express = require('express');
+const TYPE = require('./type.js');
+const get = require('./get.js');
+const initial = require('./initial.js');
+
 const cors = require('cors');
-const { response } = require('express');
+// const { response } = require('express');
 const app = express();
 
 app.disable('x-powered-by');
 app.use(cors());
+// 정적파일 저장소, express가 static 파일을 전송
+app.use('/assets', express.static('assets'));
 
 const PORT = 8000;
 let db = new sqlite3.Database( 'database.db', (err) => {
   if (!err) {
-    db.run(
-      `CREATE TABLE IF NOT EXISTS TBL_ABOUT_MYSELF (NAME TEXT, EMAIL TEXT) 
-        UNIQUE(NAME, EMAIL))`,
-      (err2) => {
-        db.run(
-          `INSERT OR IGNORE INTO TBL_ABOUT_MYSELF (NAME, EMAIL) VALUES ('DOPT', 'test@gmail.com')`
-        )
-      }
-    );
-
-    db.run(
-      `CREATE TABLE IF NOT EXISTS TBL_MY_RESUME (DATE DATE, TITLE TEXT, CONTENT TEXT, URL TEXT,
-        UNIQUE(DATE, TITLE))`,
-        (err2) => {
-          if (!err2){
-            const resume = [
-              {
-                date : '1986-01-23',
-                title : '출생',
-                content : '생일 축하 합니다~!',
-                URL : null
-              }, {
-                date : '2014-04-01',
-                title : '개발자로 시작',
-                content : '새로운 커리어로 도전',
-                URL : null
-              }, {
-                date : '2021-06-07',
-                title : 'github 시작',
-                content : '개발 관련 스터디를 위한 github 시작',
-                URL : 'https://github.com/kkkamango'
-              },          
-            ];
-    
-            resume.forEach(item => {
-              const query = `INSERT OR IGNORE INTO TBL_MY_RESUME 
-                (DATE, TITLE, CONTENT, URL) 
-                VALUES('${item.date}', '${item.title}', '${item.content}', '${item.URL}')`;
-              db.run(query);
-            });
-          }
-        }
-    );
+    initial.run(db, TYPE.about_me);
+    initial.run(db, TYPE.resume);
+    initial.run(db, TYPE.applications);
   }
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Listening... ${PORT}`);
 });
 
-app.get('/', (req, res, next) => {
-  res.json({rsp : 'ok'});
-});
-
-app.get('/db/about-me', (req, res, next) => {
-  let result = {rsp : 'fail'};
-
-  db.get("SELECT * FROM TBL_ABOUT_MYSELF", (err, row) => {
-    if (!err){
-      result.data = row;
-      db.all('SELECT * FROM TBL_MY_RESUME ORDER BY DATE DESC', (err2, rows) => {
-        if (!err2){
-          result.rsp = 'ok';
-          result.data.resume = rows;
-          res.json(result);
-        } else {
-          res.json(result);
-        }
-      });
-    } else {
-      res.json(result);
-    }
-  });
-
-  // db.get("SELECT * FROM TBL_ABOUT_MYSELF", (err, row) => {
-  //   if (!err) {
-  //     result.data = row;
-  //     db.all('SELECT * FROM TBL_MY_RESUME ORDER BY DATE DESC', (err2, rows) => {
-  //       if (!err2){
-  //         result.rsp = 'ok';
-  //         result.data.resume = rows;
-  //         res.json(result);
-  //       } else {
-  //         res.json(result);
-  //       }
-  //     });
-  //   } else {
-  //     res.json(result);
-  //   }
-  // })
-});
+get.setup(app, db);
