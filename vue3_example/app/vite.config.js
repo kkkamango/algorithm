5a61126eea7b7468b8a1d99dc9851c16b8874b05
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-14 19:18:18 +0900

    라우터 플러그인 추가

--- vue3_example/app/vite.config.js
+++ vue3_example/app/vite.config.js
@@ -7,10 +7,12 @@ export default defineConfig({
   // alias 사용
   resolve :  {
     alias : {
+      vue: 'vue/dist/vue.esm-bundler.js',
       '/@' : path.resolve(__dirname, './src'),
       '/@components' : path.resolve(__dirname, './src/components'),
       '/@app_modules' : path.resolve(__dirname, './src/modules'),
       '/@store' : path.resolve(__dirname, './src/store'),
+      '/@router' : path.resolve(__dirname, './src/router'),
     }
   },
   plugins: [vue()]
