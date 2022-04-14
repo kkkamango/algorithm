3efb469778fc975eda7a96c10e29757c4a8edb7b
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-14 19:19:16 +0900

    라우터 플러그인 추가

--- vue3_example/app/src/App.vue
+++ vue3_example/app/src/App.vue
@@ -1,17 +1,19 @@
 <script>
 import NavBar from '/@components/NavBar.vue'
-import Profile from '/@components/Profile.vue'
+// import Profile from '/@components/Profile.vue'
 
 export default {
   name : 'App',
   components : {
     NavBar,
-    Profile,
+    // Profile,
   }
 }
 </script>
 
 <template>
   <nav-bar />
-  <Profile />
+  <!-- 라우터 추가 -->
+  <router-view></router-view>
+  <!-- <Profile /> -->
 </template>
