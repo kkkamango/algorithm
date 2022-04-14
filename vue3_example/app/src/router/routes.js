99b0f26be313012c809b37d3b4e56686ed8fce60
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-14 19:23:04 +0900

    라우터 플러그인 추가

---
+++ vue3_example/app/src/router/routes.js
@@ -0,0 +1,13 @@
+import {defineComponent} from 'vue'
+import Profile from '/@components/Profile.vue'
+
+const NotFound = defineComponent({
+  template : '<div>Not Found</div>'
+});
+const routes = [
+  { path : '/', redirect : '/profile' },
+  { path : '/profile', name : 'profile', component : Profile, alias : '/home' },
+  { path : '/:catchAll(.*)+', component : NotFound },
+];
+
+export default routes;
