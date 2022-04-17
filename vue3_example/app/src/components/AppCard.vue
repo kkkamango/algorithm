f4eea935ecdd8d3056351ecf8787352eeb8c37a2
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-17 16:45:47 +0900

    5-9 어플리케이션 소개 페이지 수정

--- vue3_example/app/src/components/AppCard.vue
+++ vue3_example/app/src/components/AppCard.vue
@@ -5,14 +5,14 @@ Card를 그리는 컴포넌트
   <div class="card h-100 border-2" :class="{'border-primary' : selected_appl_id == data.id}">
     <img :src="data.image" class="card-img-top" alt="Image" />
     <div class="card-body">
-      <h5 class="card-title">{{data.NAME}}</h5>
-      <p class="card-text">{{data.CONTENT}}</p>
-      <p class="card-text" v-if="data.URL != 'null'">
+      <h5 class="card-title">{{data.name}}</h5>
+      <p class="card-text">{{data.content}}</p>
+      <p class="card-text" v-if="data.url != 'null'">
         <a :href="data.url" @click="onClickApplication(data)" target="_blank">클릭</a>
       </p>
     </div>
     <div class="card-footer">
-      <small class="text-muted">{{data.DATE}}에 등록 됨</small>
+      <small class="text-muted">{{data.date}}에 등록 됨</small>
     </div>
   </div>
 </template>
