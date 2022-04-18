4546ab27a7ec3bdd9a3b200bf2b34f5b84531dbb
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-18 09:17:32 +0900

    6-3 Vue3 Teleport를 이용한 알림 제작

--- vue3_example/app/src/App.vue
+++ vue3_example/app/src/App.vue
@@ -13,6 +13,13 @@ export default {
 
 <template>
   <nav-bar />
+  <!-- 알림이 그려질 영역
+  NavBar에서 알림을 받아 옴
+  1. provide, inject
+  2. vuex
+  3. teleport를 이용하여 App의 특정 위치에 그림
+   -->
+  <div id="notification"></div>
   <!-- 라우터 추가 -->
   <router-view></router-view>
   <!-- <Profile /> -->
