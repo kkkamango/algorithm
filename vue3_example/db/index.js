775f66092041e0c0d4a1ed313b914ffd38aada85
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-21 19:04:19 +0900

    6-5 관리자 로그인 기능

--- vue3_example/db/index.js
+++ vue3_example/db/index.js
@@ -2,6 +2,8 @@ const sqlite3 = require('sqlite3');
 const express = require('express');
 const TYPE = require('./type.js');
 const get = require('./get.js');
+const post = require('./post.js');
+const put = require('./put.js');
 const initial = require('./initial.js');
 
 const cors = require('cors');
@@ -21,6 +23,7 @@ let db = new sqlite3.Database( 'database.db', (err) => {
     initial.run(db, TYPE.applications);
     initial.run(db, TYPE.notification);
     initial.run(db, TYPE.blog);
+    initial.run(db, TYPE.accounts);
   }
 });
 
@@ -29,3 +32,5 @@ app.listen(PORT, () => {
 });
 
 get.setup(app, db);
+post.setup(app, db);
+put.setup(app, db);
