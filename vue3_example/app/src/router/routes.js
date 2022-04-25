4ffe152b72b161f5c6869f94b648da395cbe273a
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-25 19:39:01 +0900

    6-5 관리자 로그인 기능

--- vue3_example/app/src/router/routes.js
+++ vue3_example/app/src/router/routes.js
@@ -2,6 +2,7 @@ import {defineComponent} from 'vue'
 import Home from '/@components/Home.vue'
 import Application from '/@components/Application.vue'
 import Profile from '/@components/Profile.vue'
+import Admin from '/@components/Admin.vue'
 
 const NotFound = defineComponent({
   template : '<div>Not Found</div>'
@@ -10,6 +11,7 @@ const routes = [
   { path : '/', redirect : '/home' },
   { path : '/home', name : 'home', component : Home },
   { path : '/profile', name : 'profile', component : Profile },
+  { path : '/admin', name : 'admin', component : Admin },
   { path: '/application', name: 'application', component: Application },
   { path : '/:catchAll(.*)+', component : NotFound },
 ];
