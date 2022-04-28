56736b8d2b4982b6c1652c2e95df1bfdb7d6cb52
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-28 16:21:50 +0900

    6-6 markdown을 이용한 글쓰기 기능 추가

--- vue3_example/app/src/components/Blog.vue
+++ vue3_example/app/src/components/Blog.vue
@@ -1,147 +1,99 @@
-<template>
-<!--
-main                    -- 컨테이너
-  article               -- 포스트
-    h2#blog-post-title
-    p#blog-post-meta
-  nav                   -- 페이징
-  about
-  archives              -- 월별 아카이브
--->
-  <main class="container mb-3">
-    <div class="row g-5">
-      <div class="col-md-8">
-        <h3 class="pb-4 md-4 fst-italic border-bottom">
-          Blog
-        </h3>
-        <!-- posts -->
-        <article v-for="article in sliced_posts" :key="article.id" class="blog-post"
-          :id="'article-' + article.id">
-          <h2 class="blog-post-title">{{article.title}}</h2>
-          <p class="blog-post-meta">{{article.date}}</p>
-          <div v-html="article.post"></div>
-        </article>
 
-        <!-- pagination -->
-        <nav class="blog-pagination" aria-label="Pagination">
-          <a href="#" class="btn"
-            :class="[page == 1 ? 'btn-outline-secondary disable' : 'btn-outline-primary']"
-            @click="page--">Older</a>
+    hipassTokenList : [], 
+    skpToken : '', // 레몬패스 결제시 토큰정보
 
-          {{page}}
 
-          <a href="#" class="btn"
-            :class="[page == total_pages ? 'btn-outline-secondary disable' : 'btn-outline-primary']"
-            @click="page++">Newer</a>
-        </nav>
-      </div>
 
-      <div class="col-md-4">
-        <div class="position-sticky" style="top: 2rem;">
-          <div class="p-4 mb-3 bg-light rounded">
-            <h4 class="fst-italic">About</h4>
-            <p class="mb-0">Customize this section to tell your visitors a little bit about your publication, writers, content, or something else entirely. Totally up to you.</p>
-          </div>
 
-          <!-- 아카이브 -->
-          <div class="p-4">
-            <h4 class="fst-italic">Archives</h4>
-            <ol v-for="(archive, index) in archives" :key="index" class="list-unstyled mb-0">
-              <li>
-                <a :data-bs-target="'#archive-' + index"
-                  data-bs-toggle="collapse" href="#">{{archive.key}}</a>
-                <div class="collapse" :id="'archive-' + index">
-                  <ol class="list-unstyled ms-3">
-                    <li v-for="post in archive.posts" :key="post.id" >
-                      {{post.date.substring(0, 10) + ': '}}
-                      <a :href="'#article-' + post.id"
-                        @click="onArchive($event, post.id)">{{post.title.substring(0, 20)}}</a>
-                    </li>
-                  </ol>
+<select class="basic_input browser-default w100p" v-model="skpToken">
+    <option value="" disabled selected>카드 선택</option>
+    <template v-for="token in hipassTokenList">
+        <option value="token">{{token.cardNm}}</option>
+    </template>
+</select>
+
+
+
+/**
+    * KOCES_LEMON_PASS 결제 프로세스 영역
+    */
+startKocesLemonPassPaymentProcess : function () {
+    /*TODO
+        skpToken 선택여부,
+        토큰 유효기간,
+            유효기간 만료시 ?? 다른 토큰 선택 또는 토큰 등록페이지로 이동??
+        비밀번호 등록여부,
+            미등록시 등록 팝업 ?
+        비밀번호 입력
+        로딩바 1/n
+        승인요청
+            validation 토큰유효성, 비밀번호 검증
+            승인 -> 수납알림
+        */
+    if (!this.skpToken) {
+        this.$commonAlert(`결제할 카드를 선택해 주세요.`);
+        return false;
+    } else if (new Date(this.$dateFormatter(this.skpToken.endDt, 'yyyy-mm-dd')) < new Date()){
+        this.$commonAlert(`등록한 카드에 결제가능한 기간을 확인해 주세요.`);
+        // TODO 다른 토큰 선택 or 토큰 등록 페이지로 이동
+        return false;
+    }
+    // TODO 비밀번호 등록 여부
+    // 미등록시
+    // 비밀번호 입력 팝업
+    let options = {
+        title: '<a href="javascript:;">토큰 비밀번호 등록</a>',
+        text: `<div class="data_ip_area">
+                    <div class="input-field col s12">
+                        <input id="token_password" type="password" maxlength="20">
+                        <label for="token_password" class="active">비밀번호</label>
+                    </div>
+                    <div class="input-field col s12">
+                        <input id="token_password_check" type="password" maxlength="20">
+                        <label for="token_password_check" class="active">비밀번호 확인</label>
+                        </div>
+                    </div>`,
+        confirm: true,
+        confirmTxt: '저장',
+        callback: function() {
+            if (this.validateTokenPassword()){
+                const tokenPasswordParam = {
+                    patientId : this.patientId,
+                    cardToken : this.skpToken,
+                }
+                this.$customAxios('', tokenPasswordParam, this.getKocesHashDataCallback);
+            }
+        },
+        cancel: true,
+        cancelTxt: '취소',
+        cancelCallback: function() {
+            this.skpToken = '';
+        },
+        dismissible: true
+    };
+    Vue.prototype.$mcareModal(options);
+    M.updateTextFields();
+
+    let payOptions = {
+    title: '<a href="javascript:;">결제 비밀번호 확인</a>',
+    text: `<div class="data_ip_area">
+                <div class="input-field col s12">
+                    <input id="approval_token_password" type="number" pattern="[0-9]*" inputmode="numeric" 
+                        maxlength="6" oninput="Vue.prototype.$maxLengthCheck(this)" class="number_password">
+                    <label for="approval_token_password" class="active">비밀번호</label>
                 </div>
-              </li>
-            </ol>
-          </div>
-
-          <div class="p-4">
-            <h4 class="fst-italic">Elsewhere</h4>
-            <ol class="list-unstyled">
-              <li><a href="#">GitHub</a></li>
-              <li><a href="#">Twitter</a></li>
-              <li><a href="#">Facebook</a></li>
-            </ol>
-          </div>
-        </div>
-      </div>
-    </div>
-  </main>
-
-</template>
-
-<script>
-import useAxios from '/@app_modules/axios.js'
-import {ref, reactive, computed, onMounted} from 'vue'
-
-export default {
-  setup() {
-    const {axiosGet} = useAxios();
-    const posts = reactive([]);
+                </div>`,
+    confirm: true,
+    confirmTxt: '확인',
+    callback: function() {
     
-    // 페이징
-    const rows = ref(5); // 한페이지 보여지는 post 수
-    const total_rows = ref(0); // 전체 post 수
-    const page = ref(1); // 현재 페이지
-    const total_pages = computed(() => { // 전체 페이지
-      return Math.ceil(total_rows.value / rows.value)
-    })
-    const sliced_posts = computed(() => { // 화면에 보여지는 post
-      return posts.slice( (page.value -1) * rows.value, page.value * rows.value )
-    })
-    
-    // 아카이브 (월별)
-    const archives = reactive([]);
-    // 아카이브에서 포스트 클릭시 article id 획득
-    const onArchive = (e, id) => {
-      const index = posts.findIndex( post => post.id == id ) + i;
-      page.value = Math.ceil(index / rows.value);
-    }
-    
-    onMounted(() => {
-      axiosGet('/db/blog', onSucces)
-    })
-
-    const onSucces = (data) =>{
-      Object.assign(posts, data.data);
-      total_rows.value = posts.length;
-
-      // 아카이브 (월별)
-      const temp_group = posts.reduce((accumulator, currentValue) => {
-        ;(accumulator[currentValue['date'].substring(0,7)] =
-        accumulator[currentValue['date'].substring(0, 7)] || []).push(currentValue)
-        return accumulator
-      }, {})
-
-      Object.assign(
-        archives, 
-        Object.keys(temp_group).map((key) => ({
-          key : key,
-          posts : temp_group[key],
-        }))
-      )
-    }
-
-    return {
-      // posts,
-      sliced_posts,
-      page,
-      total_pages,
-      archives,
-      onArchive,
-    }
-  }  
-}
-</script>
-
-<style scoped>
-
-</style>
+    },
+    cancel: true,
+    cancelTxt: '취소',
+    cancelCallback: function() {
+        this.skpToken = '';
+    },
+    dismissible: true
+};
+Vue.prototype.$mcareModal(payOptions);
+M.updateTextFields();
