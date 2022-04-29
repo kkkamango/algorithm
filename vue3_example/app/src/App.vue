8b5aa415a8f135330d7ce0a8aa653063c63a9718
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-29 17:07:13 +0900

    7-2 Toast 컴포넌트

--- vue3_example/app/src/App.vue
+++ vue3_example/app/src/App.vue
@@ -1,17 +1,26 @@
 <script>
 import NavBar from '/@components/NavBar.vue'
+import Toast from '/@components/Toast.vue'
+import { ref, provide } from 'vue'
 // import Profile from '/@components/Profile.vue'
 
 export default {
   name : 'App',
+  setup(props) {
+    const toast = ref('');
+    // provide 하위 모든 컴포넌트에게 같은 값 제공
+    provide('toast', toast);
+  },
   components : {
     NavBar,
+    Toast,
     // Profile,
   }
 }
 </script>
 
 <template>
+  <toast />
   <nav-bar />
   <!-- 알림이 그려질 영역
   NavBar에서 알림을 받아 옴
