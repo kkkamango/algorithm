3a0f31528572816ae5ab9c3de95bee1add7d0758
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-29 15:14:10 +0900

    7-1 MD5 비밀번호 보호

--- vue3_example/app/src/modules/login.js
+++ vue3_example/app/src/modules/login.js
@@ -1,12 +1,13 @@
 import useAxios from '/@app_modules/axios.js'
 const { axiosPut, axiosPost } = useAxios()
+import md5 from 'js-md5'
 
 export default function(){
   // 세션과 같이 관리자가 접속중인지 확인
   const checkToken = (email, token) => 
     new Promise((resolve, reject) => {
       axiosPost(
-        `/db/accounts/check-token/${email}/${token}`, 
+        `/db/accounts/check-token/${md5(email)}/${token}`, 
         {},
         (data) => {
           resolve(data);
@@ -19,8 +20,9 @@ export default function(){
   
   const updatePassword = (email, password, oldpassword) =>
     new Promise((resolve, reject) => {
+      const enc_pw = password === 'vue' ? password : md5(password)
       axiosPut(
-        `/db/accounts/${email}/${password}/${oldpassword}`, 
+        `/db/accounts/${md5(email)}/${enc_pw}/${md5(oldpassword)}`, 
         {},
         (data) => {
           resolve(data);
@@ -34,7 +36,7 @@ export default function(){
   const login = (email, password) => 
     new Promise((resolve, reject) => {
       axiosPost(
-        `/db/accounts/login/${email}/${password}`, 
+        `/db/accounts/login/${md5(email)}/${md5(password)}`, 
         {},
         (data) => {
           resolve(data);
