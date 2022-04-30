288d0c93313f4bcc8d2e985ca20c4773e5f1fe45
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-30 16:37:31 +0900

    7-5 vuex 컴포지션 API 로 변경

--- vue3_example/app/src/main.js
+++ vue3_example/app/src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import {store} from '/@store/index.js'
+// import {store} from '/@store/index.js'
 import {router} from '/@router/router.js'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.esm.js'
 
 const app = createApp(App);
-app.use(store); // 플러그인 등록
+// app.use(store); // 플러그인 등록
 app.use(router); // 라우터 등록
 app.mount('#app')
