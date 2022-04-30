795d07b8b14fa4c488e525e3fff6620f794c2b22
kkkamango <85475328+kkkamango@users.noreply.github.com>
2022-04-30 16:33:51 +0900

    7-5 vuex 컴포지션 API 로 변경

--- vue3_example/app/vite.config.js
+++ vue3_example/app/vite.config.js
@@ -10,11 +10,13 @@ export default defineConfig({
       vue: 'vue/dist/vue.esm-bundler.js',
       '/@' : path.resolve(__dirname, './src'),
       '/@components' : path.resolve(__dirname, './src/components'),
+      '/@compositions': path.resolve(__dirname, './src/compositions'),
       '/@app_modules' : path.resolve(__dirname, './src/modules'),
       '/@store' : path.resolve(__dirname, './src/store'),
       '/@router' : path.resolve(__dirname, './src/router'),
       '/@utils' : path.resolve(__dirname, './src/modules'),
-    }
+    },
+    extensions : ['.js'] // 확장자를 쓰지 않아도 됨
   },
   plugins: [vue()]
 })
